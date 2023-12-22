1376588fdaa471f1fb50327040a753f3e4f1cf47
Antonio Thomacelli Gomes <tonnytg@gmail.com>
2023-12-21 23:42:53 -0300

    Update megaphone.cpp
    improve code

--- Module00/ex00/megaphone.cpp
+++ Module00/ex00/megaphone.cpp
@@ -4,49 +4,42 @@
 
 class Argument {
 private:
-	std::string _value;
+    std::string _value;
 
 public:
-	Argument() {}
-
-	Argument(const char* arg) : _value(arg) {}
-
-	Argument(const Argument& other) : _value(other._value) {}
-
-	Argument& operator=(const Argument& other) {
-		if (this != &other) {
-			_value = other._value;
-		}
-		return *this;
-	}
-	~Argument() {}
-
-	void toUpper() {
-		for (size_t i = 0; i < _value.size(); i++) {
-			_value[i] = std::toupper(_value[i]);
-		}
-	}
-
-	const std::string getValue() const {
-		return _value;
-	}
+    Argument() = default;
+    Argument(const char* arg) : _value(arg) {}
+    Argument(const Argument& other) : _value(other._value) {}
+    Argument& operator=(const Argument& other) {
+        if (this != &other) {
+            _value = other._value;
+        }
+        return *this;
+    }
+    ~Argument() = default;
+    void toUpper() {
+        for (size_t i = 0; i < _value.size(); i++) {
+            _value[i] = std::toupper(_value[i]);
+        }
+    }
+    const std::string& getValue() const {
+        return _value;
+    }
 };
 
-int main(int argc, char **argv) {
-	if (argc < 2) {
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-		return 1;
-	}
+int main(int argc, char **argv)
+{
+    if (argc < 2) {
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+        return 1;
+    }
 
-	for (int i = 1; i < argc; i++) {
-		Argument arg(argv[i]);
-		arg.toUpper();
-		std::cout << arg.getValue() << " ";
-	}
-	std::cout << std::endl;
+    for (int i = 1; i < argc; i++) {
+        Argument arg(argv[i]);
+        arg.toUpper();
+        std::cout << arg.getValue() << ' ';
+    }
+    std::cout << '\n';
 
-	// Chama todos os destrutores
-	// ...
-
-	return 0;
+    return 0;
 }
