cc747396162cb242b3910041b13759a821d99909
Antonio Thomacelli <tonnytg@gmail.com>
2024-02-13 23:16:24 -0300

    feat: organize code and functions

--- Module01/ex04/main.cpp
+++ Module01/ex04/main.cpp
@@ -3,7 +3,7 @@
 #include <sstream>
 #include <string>
 
-std::string replaceAll(std::string str, const std::string& from, const std::string& to) {
+std::string replaceAll(std::string str, const std::string &from, const std::string &to) {
     size_t start_pos = 0;
     std::string result;
 
@@ -21,15 +21,15 @@ std::string replaceAll(std::string str, const std::string& from, const std::stri
     return result;
 }
 
-int main(int argc, char **argv) {
-    if (argc != 4) {
-        std::cerr << "Usage: " << argv[0] << " <filename> <s1> <s2>" << std::endl;
-        return 1;
-    }
+std::string newFileName(std::string filename) {
 
-    std::string filename = argv[1];
-    std::string s1 = argv[2];
-    std::string s2 = argv[3];
+	size_t lastDotPos = filename.find_last_of(".");
+	std::string newFilename = filename.substr(0, lastDotPos);
+
+	return newFilename;
+}
+
+int	replaceFile(std::string filename, std::string s1, std::string s2) {
 
     std::ifstream inFile(filename.c_str());
     if (!inFile) {
@@ -44,7 +44,7 @@ int main(int argc, char **argv) {
 
     std::string modifiedContent = replaceAll(content, s1, s2);
 
-    std::ofstream outFile((filename + ".replace").c_str());
+    std::ofstream outFile((newFileName(filename) + ".replace").c_str());
     if (!outFile) {
         std::cerr << "Cannot create output file." << std::endl;
         return 1;
@@ -52,6 +52,25 @@ int main(int argc, char **argv) {
 
     outFile << modifiedContent;
     outFile.close();
+
+	return 0;
+}
+
+int main(int argc, char **argv) {
+
+    if (argc != 4) {
+        std::cerr << "Usage: " << argv[0] << " <filename> <s1> <s2>" << std::endl;
+        return 1;
+    }
+
+    std::string filename = argv[1];
+    std::string s1 = argv[2];
+    std::string s2 = argv[3];
+
+	if (replaceFile(filename, s1, s2)) {
+		std::cout << "ERROR to replace file" << std::endl;
+		return 1;
+	}
 
     return 0;
 }
