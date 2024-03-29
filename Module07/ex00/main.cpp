4427e2b4e1394b41c41bec723bcce85d594abfe4
Antonio Thomacelli <tonnytg@gmail.com>
2024-03-29 19:14:09 -0300

    feat: add example to fix template or generics terms

---
+++ Module07/ex00/main.cpp
@@ -0,0 +1,27 @@
+#include "Whatever.hpp"
+
+int main( void ) {
+
+	{
+    	int a = 2;
+    	int b = 3;
+    	std::cout << "a = " << a << ", b = " << b << std::endl;
+		std::cout << "::swap( a, b )" << std::endl;
+    	::swap( a, b );
+    	std::cout << "a = " << a << ", b = " << b << std::endl;
+    	std::cout << "::min( a, b )" << " = " << ::min( a, b ) << std::endl;
+    	std::cout << "::max( a, b )" << " = " << ::max( a, b ) << std::endl;
+	}
+	{
+		std::string c = "lettera";
+		std::string d = "letterb";
+		std::cout << "c = " << c << ", d = " << d << std::endl;
+		std::cout << "::swap( c, d )" << std::endl;
+		::swap(c, d);
+		std::cout << "c = " << c << ", d = " << d << std::endl;
+		std::cout << "::min( c, d )" << " = " << ::min( c, d ) << std::endl;
+		std::cout << "::max( c, d )" << " = " << ::max( c, d ) << std::endl;
+	}
+
+    return (0);
+}
